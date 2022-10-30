22469fe61b96f364038e98d212c44a796712dfbd
Cleiane Clementino Bondade <cleiane.bondade@ufrn.br>
2022-10-30 18:31:27 -0300

    Apagar comentários desnecessários

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+//Apagar depois dos testes, está aqui só para facilitar os testes da tela
 import 'package:buscapatas/cadastros/cadastro-post.dart';
 import 'package:flutter/material.dart';
+//Verificar a necessidade de apagar depois dos testes, se der para manter sessão pode iniciar na home em vez de login
 import 'package:buscapatas/home.dart';
 import 'package:material_color_generator/material_color_generator.dart';
 import 'package:buscapatas/componentes-interface/estilo.dart' as estilo;
 import 'package:buscapatas/publico/login.dart';
+//Apagar depois dos testes, está aqui só para facilitar os testes da tela
 import 'package:buscapatas/publico/nao-implementado.dart';
 
 void main() {
@@ -13,26 +16,17 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Busca Patas',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: generateMaterialColor(color: estilo.corsecundaria),
       ),
       //home: CadastroPost(title: "Cadastro de animal"),
       home: Home(true, title: "Página Inicial"),
-      //home: NaoImplementado(title: "Login - BuscaPatas"),
+      //home: Login(title:"Login - BuscaPatas"),
+      //home: NaoImplementado(title: "Não implementado"),
     );
   }
 }
